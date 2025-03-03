695cbf08af0bd9f06ca7b40952f24eb8bdb53ace
OndraHoli <holecekondrej1@gmail.com>
2025-03-03 10:23:34 +0100

    funguje scitani a parsovani

--- src/main/java/pro1/Fraction.java
+++ src/main/java/pro1/Fraction.java
@@ -13,13 +13,32 @@ public class Fraction {
 
 
     }
+    public Fraction add(Fraction other) {
+        //scitani dvou zlomku
+        return new Fraction(n*other.d + other.n * d, d*other.d); // pristup o ohter d a n mam i kdyz jsou private, protoze jsou e stejne tridy??
+    }
     public static Fraction parse(String str) {
-        return new Fraction(1,1);
+        str = str.replace(" ","");
+        String[] parts = str.split("\\+"); // 2 zpetne lomitka
+
+        Fraction sum = new Fraction(0,1);
+        Fraction partFraction;
+        for(String part : parts){
+            //prevest na fractiony a secist
+            if(part.contains("%")){
+                part = part.replace("%","");
+                partFraction = new Fraction(Long.parseLong(part),100);
+            }
+            else{
+                String[] frac = part.split("/");
+                partFraction = new Fraction(Long.parseLong(frac[0]),Long.parseLong(frac[1]));
+            }
+            sum = sum.add(partFraction);
+        }
+        return sum;
     }
     public String toString(){
         return n + "/" + d;
     }
-
-
 
 }
